44addd18e6693dd6cd535d4fc242ac9a4de44801
HelenRaven <HelenRaven@mail.ru>
2024-12-21 22:42:11 +0700

    Initial commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Server started");
+        int port = 8089;
+
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            while(true){
+                try(Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                ){
+                    System.out.printf("New connection accepted. Port: %d%n", clientSocket.getPort());
+                    final String name = in.readLine();
+                    out.println(String.format("Hi %s, your port %d", name, clientSocket.getPort()));
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
